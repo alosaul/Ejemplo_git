7fb87fcfb1f9352d9f523f360fc395641e6ae5ce
DAW <DAW@PC5054>
2018-11-05 21:16:25 +0100

    php_2

--- index.php
+++ index.php
@@ -13,6 +13,11 @@ and open the template in the editor.
         <?php
         // put your code here
         echo 'hola mundo';
+        
+        if(5>3){
+            echo 'pedro';
+            
+        }
         ?>
     </body>
 </html>
